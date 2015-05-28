f1e121ab2c18d66761654a0fb60029241b00b544
sciooga <huguotao@t.local>
2015-05-28 11:15:09 +0800

    update img js and css

--- launch.js
+++ launch.js
@@ -60,7 +60,6 @@ function useSetting(){
     now_searchEngine == 'Other' && (now_searchEngineURL = getCookie('searchEngineURL')) && (now_searchEnginePOST = getCookie('searchEnginePOST'));
     now_backgroundURL = getCookie('backgroundURL');
     var _keyboardBg = $('#keyboardBg');
-    now_backgroundURL && _keyboardBg.attr('src',getCookie('backgroundURL')) && _keyboardBg.fadeTo(2000,1) || _keyboardBg.fadeTo(2000,0);
 }
 /*-----------USESETTING END-----------*/
 
@@ -83,9 +82,59 @@ for (var i = 48; i < 91; i++) {
 
 /*-----------ISURL START-----------*/
 function IsURL(a) {
-    var b = "^((https|http|ftp|rtsp|mms)?://)" + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" + "(([0-9]{1,3}.){3}[0-9]{1,3}" + "|" + "([0-9a-z_!~*'()-]+.)*" + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]." + "[a-z]{2,6})" + "(:[0-9]{1,4})?" + "((/?)|" + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
-    var c = new RegExp(b);
-    return !!c.test(a);
+    //URL have to has '.' or '。'
+    if (!RegExp('[.。]').test(a)){ return false; }
+    //URL can`t have any space
+    if (RegExp(' ').test(a)){ return false; }
+    //URL have to start with letter,digital or chinese simplified character
+    if (!RegExp('^[A-Za-z0-9\u4e00-\u9fa5]').test(a)){
+        return false;
+        }
+    //common domain name
+    if (RegExp('.+[.。](com|cn|org|net|info|club|edu|gov|io)/?(/.+)?$').test(a)){ return true; }
+    //IP address
+    if (RegExp('^((http|https)://)?((2[0-4][0-9]|25[0-5]|[01]?[0-9][0-9]?)[.。]){3}(2[0-4][0-9]|25[0-5]|[01]?[0-9][0-9]?)/?([:/].+)?$').test(a)){ return true; }
+    //have chinese simplified character but not domain name
+    if (RegExp('[\u4e00-\u9fa5]').test(a)){
+        if (RegExp('.+[.。](测试|佛山|慈善|集团|在线|八卦|公益|公司|移动|我爱你|时尚|淡马锡|商标|商店|商城|工行|中文网|中信|中国|中國|娱乐|谷歌|測試|网店|网络|香港|飞利浦|台湾|台灣|手机|澳門|政府|机构|组织机构|健康|世界|网址|游戏|企业|信息|广东|新加坡|政务)/?(/.+)?$').test(a)){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+//all of domain name
+//this also work quickly...
+switch(RegExp('.+[.。](.)').exec(a)[1]){
+/*A*/case 'a':    if (RegExp('.+[.。](abb|abbott|abogado|ac|academy|accenture|accountant|accountants|active|actor|ad|ads|adult|ae|aero|af|afl|ag|agency|ai|aig|airforce|al|allfinanz|alsace|am|amsterdam|an|android|ao|apartments|aq|aquarelle|ar|archi|army|arpa|as|asia|associates|at|attorney|au|auction|audio|auto|autos|aw|ax|axa|az)/?(/.+)?$').test(a)){ return true; }; break;
+/*B*/case 'b':    if (RegExp('.+[.。](ba|band|bank|bar|barclaycard|barclays|bargains|bauhaus|bayern|bb|bbc|bd|be|beer|berlin|best|bf|bg|bh|bi|bid|bike|bingo|bio|biz|bj|bl|black|blackfriday|bloomberg|blue|bm|bmw|bn|bnpparibas|bo|boats|bond|boo|boutique|bq|br|bridgestone|broker|brother|brussels|bs|bt|budapest|build|builders|business|buzz|bv|bw|by|bz|bzh)/?(/.+)?$').test(a)){ return true; }; break;
+/*C*/case 'c':    if (RegExp('.+[.。](ca|cab|cafe|cal|camera|camp|cancerresearch|canon|capetown|capital|caravan|cards|care|career|careers|cars|cartier|casa|cash|casino|cat|catering|cbn|cc|cd|center|ceo|cern|cf|cfa|cfd|cg|ch|channel|chat|cheap|chloe|christmas|chrome|church|ci|cisco|citic|city|ck|cl|claims|cleaning|click|clinic|clothing|club|cm|cn|co|coach|codes|coffee|college|cologne|com|community|company|computer|condos|construction|consulting|contractors|cooking|cool|coop|corsica|country|coupons|courses|cr|credit|creditcard|cricket|crs|cruises|cu|cuisinella|cv|cw|cx|cy|cymru|cyou|cz)/?(/.+)?$').test(a)){ return true; }; break;
+/*D*/case 'd':    if (RegExp('.+[.。](dabur|dad|dance|date|dating|datsun|day|dclk|de|deals|degree|delivery|democrat|dental|dentist|desi|design|dev|diamonds|diet|digital|direct|directory|discount|dj|dk|dm|dnp|do|docs|dog|doha|domains|doosan|download|durban|dvag|dz)/?(/.+)?$').test(a)){ return true; }; break;
+/*E*/case 'e':    if (RegExp('.+[.。](earth|eat|ec|edu|education|ee|eg|eh|email|emerck|energy|engineer|engineering|enterprises|epson|equipment|er|erni|es|esq|estate|et|eu|eurovision|eus|events|everbank|exchange|expert|exposed|express)/?(/.+)?$').test(a)){ return true; }; break;
+/*F*/case 'f':    if (RegExp('.+[.。](fail|faith|fan|fans|farm|fashion|feedback|fi|film|finance|financial|firmdale|fish|fishing|fit|fitness|fj|fk|flights|florist|flowers|flsmidth|fly|fm|fo|foo|football|forex|forsale|foundation|fr|frl|frogans|fund|furniture|futbol|fyi)/?(/.+)?$').test(a)){ return true; }; break;
+/*G*/case 'g':    if (RegExp('.+[.。](ga|gal|gallery|garden|gb|gbiz|gd|gdn|ge|gent|gf|gg|ggee|gh|gi|gift|gifts|gives|gl|glass|gle|global|globo|gm|gmail|gmo|gmx|gn|gold|goldpoint|golf|goo|goog|google|gop|gov|gp|gq|gr|graphics|gratis|green|gripe|gs|gt|gu|guge|guide|guitars|guru|gw|gy)/?(/.+)?$').test(a)){ return true; }; break;
+/*H*/case 'h':    if (RegExp('.+[.。](hamburg|hangout|haus|healthcare|help|here|hermes|hiphop|hitachi|hiv|hk|hm|hn|hockey|holdings|holiday|homes|honda|horse|host|hosting|house|how|hr|ht|hu)/?(/.+)?$').test(a)){ return true; }; break;
+/*I*/case 'i':    if (RegExp('.+[.。](ibm|icbc|icu|id|ie|ifm|il|im|immo|immobilien|in|industries|infiniti|info|ing|ink|institute|insure|int|international|investments|io|iq|ir|irish|is|it|iwc)/?(/.+)?$').test(a)){ return true; }; break;
+/*J*/case 'j':    if (RegExp('.+[.。](java|jcb|je|jetzt|jewelry|jll|jm|jo|jobs|joburg|jp|juegos)/?(/.+)?$').test(a)){ return true; }; break;
+/*K*/case 'k':    if (RegExp('.+[.。](kaufen|kddi|ke|kg|kh|ki|kim|kitchen|kiwi|km|kn|koeln|komatsu|kp|kr|krd|kred|kw|ky|kyoto|kz)/?(/.+)?$').test(a)){ return true; }; break;
+/*L*/case 'l':    if (RegExp('.+[.。](la|lacaixa|land|lat|latrobe|lawyer|lb|lc|lds|lease|leclerc|legal|lgbt|li|liaison|lidl|life|lighting|limited|limo|link|lk|loan|loans|lol|london|lotte|lotto|love|lr|ls|lt|ltda|lu|lupin|luxe|luxury|lv|ly)/?(/.+)?$').test(a)){ return true; }; break;
+/*M*/case 'm':    if (RegExp('.+[.。](ma|madrid|maif|maison|management|mango|market|marketing|markets|marriott|mba|mc|md|me|media|meet|melbourne|meme|memorial|men|menu|mf|mg|mh|miami|mil|mini|mk|ml|mm|mma|mn|mo|mobi|moda|moe|monash|money|mormon|mortgage|moscow|motorcycles|mov|movie|mp|mq|mr|ms|mt|mtn|mtpc|mu|museum|mv|mw|mx|my|mz)/?(/.+)?$').test(a)){ return true; }; break;
+/*N*/case 'n':    if (RegExp('.+[.。](na|nadex|nagoya|name|navy|nc|ne|nec|net|network|neustar|new|news|nexus|nf|ng|ngo|nhk|ni|nico|ninja|nissan|nl|no|np|nr|nra|nrw|ntt|nu|nyc|nz)/?(/.+)?$').test(a)){ return true; }; break;
+/*O*/case 'o':    if (RegExp('.+[.。](okinawa|om|one|ong|onl|online|ooo|org|organic|osaka|otsuka|ovh)/?(/.+)?$').test(a)){ return true; }; break;
+/*P*/case 'p':    if (RegExp('.+[.。](pa|page|panerai|paris|partners|parts|party|pe|pf|pg|ph|pharmacy|philips|photo|photography|photos|physio|piaget|pics|pictet|pictures|pink|pizza|pk|pl|place|plumbing|plus|pm|pn|pohl|poker|porn|post|pr|praxi|press|pro|prod|productions|prof|properties|property|ps|pt|pub|pw|py)/?(/.+)?$').test(a)){ return true; }; break;
+/*Q*/case 'q':    if (RegExp('.+[.。](qa|qpon|quebec)/?(/.+)?$').test(a)){ return true; }; break;
+/*R*/case 'r':    if (RegExp('.+[.。](racing|re|realtor|recipes|red|redstone|rehab|reise|reisen|reit|ren|rent|rentals|repair|report|republican|rest|restaurant|review|reviews|rich|rio|rip|ro|rocks|rodeo|rs|rsvp|ru|ruhr|run|rw|ryukyu)/?(/.+)?$').test(a)){ return true; }; break;
+/*S*/case 's':    if (RegExp('.+[.。](sa|saarland|sale|samsung|sap|sarl|saxo|sb|sc|sca|scb|schmidt|scholarships|school|schule|schwarz|science|scot|sd|se|seat|sener|services|sew|sex|sexy|sg|sh|shiksha|shoes|show|shriram|si|singles|site|sj|sk|sky|sl|sm|sn|so|soccer|social|software|sohu|solar|solutions|sony|soy|space|spiegel|spreadbetting|sr|ss|st|study|style|su|sucks|supplies|supply|support|surf|surgery|suzuki|sv|swiss|sx|sy|sydney|systems|sz)/?(/.+)?$').test(a)){ return true; }; break;
+/*T*/case 't':    if (RegExp('.+[.。](taipei|tatar|tattoo|tax|taxi|tc|td|team|tech|technology|tel|temasek|tennis|tf|tg|th|thd|theater|tickets|tienda|tips|tires|tirol|tj|tk|tl|tm|tn|to|today|tokyo|tools|top|toray|toshiba|tours|town|toys|tp|tr|trade|trading|training|travel|trust|tt|tui|tv|tw|tz)/?(/.+)?$').test(a)){ return true; }; break;
+/*U*/case 'u':    if (RegExp('.+[.。](ua|ug|uk|um|university|uno|uol|us|uy|uz)/?(/.+)?$').test(a)){ return true; }; break;
+/*V*/case 'v':    if (RegExp('.+[.。](va|vacations|vc|ve|vegas|ventures|versicherung|vet|vg|vi|viajes|video|villas|vision|vlaanderen|vn|vodka|vote|voting|voto|voyage|vu)/?(/.+)?$').test(a)){ return true; }; break;
+/*W*/case 'w':    if (RegExp('.+[.。](wales|wang|watch|webcam|website|wed|wedding|weir|wf|whoswho|wien|wiki|williamhill|win|wme|work|works|world|ws|wtc|wtf)/?(/.+)?$').test(a)){ return true; }; break;
+/*X*/case 'x':    if (RegExp('.+[.。](xerox|xin|xxx|xyz)/?(/.+)?$').test(a)){ return true; }; break;
+/*Y*/case 'y':    if (RegExp('.+[.。](yachts|yandex|ye|yodobashi|yoga|yokohama|youtube|yt)/?(/.+)?$').test(a)){ return true; }; break;
+/*Z*/case 'z':    if (RegExp('.+[.。](za|zip|zm|zone|zuerich|zw)/?(/.+)?$').test(a)){ return true; }; break;
+}
+
+    return false;
 }
 /*-----------ISURL END-----------*/
 
@@ -134,7 +183,7 @@ $(document).ready(function(){
                             }
                             if (keyCode == 13){
                                 var searchTXT = _search.val();
-                                IsURL(searchTXT) && !openURL(buildURL(searchTXT)) || searchTXT && openURL(now_searchEngineURL+'?'+now_searchEnginePOST+'='+_search.val());
+                                IsURL(searchTXT.toLowerCase()) && !openURL(buildURL(searchTXT)) || searchTXT && openURL(now_searchEngineURL+'?'+now_searchEnginePOST+'='+_search.val());
                             }
                             //keyCode == 229 when use IME
                             if (keyCode == 229){
